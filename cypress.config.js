d62589109ab87f422ad2776073da563c607eba9c
allen <allen@nextfinancial.io>
2022-11-08 15:43:04 +0800

    modified cypress test configuration

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "https://jupiter.cloud.planittesting.com/#/",
+    viewportHeight: 960,
+    viewportWidth: 1536,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 20000,
+    requestTimeout: 10000,
+    responseTimeout: 10000,
+    video:false
   },
 });
